ae47043b534813c2a748a4b1d5452fccd2e079c5
Will Loughlin <54918966+WillLoughlin@users.noreply.github.com>
2021-02-15 20:42:22 -0700

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,21 +1,50 @@
-int** CreateBoard(int* arr[3][3])
+#include <iostream>
+
+int** CreateBoard()
 {
+  int **arr = 0;
+  arr = new int*[3];
   for(int i = 0; i < 3; i++)
   {
+    arr[i] = new int[3];
     for(int y = 0; y < 3; y++)
     {
       arr[i][y] = -1;
     }
-    
+  }
+
    return arr;
 
+}
+
+void DisplayBoard(int **arr)
+{
+  for (int x = 0; x < 3; x++){
+    for (int y = 0; y < 3; y++){
+      if (arr[x][y] == 0){
+        std::cout << "O";
+      } else if (arr[x][y] == 1){
+        std::cout << "X";
+      } else {
+        std::cout << " ";
+      }
+      if (y != 2){
+        std::cout << "|";
+      }
+    }
+    std::cout << "" << std::endl;
+    if (x != 2){
+      std::cout << "-----";
+    }
+    std::cout << "" << std::endl;
+
+  }
 }
 
 
 int main()
 {
-  int arr_[3][3] = new int [][];
-  arr_ = CreateBoard(arr_);
-  
+  int **arr_ = CreateBoard();
+  DisplayBoard(arr_);
 
 }
